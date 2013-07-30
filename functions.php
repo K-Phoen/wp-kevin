8da4ddc43445566057cb4e1a79e3f887d3931fdc
kphoen <geek63@gmail.com>
2013-07-30 11:55:35 +0200

    Chg: the readmore link is now under the excerpt

--- functions.php
+++ functions.php
@@ -1,7 +1,7 @@
 <?php
-function new_excerpt_more( $more ) {
-    return ' &hellip; <a href="'. get_permalink() . '">Lire la suite &rarr;</a>';
+function new_excerpt_more($more) {
+    return ' &hellip; <a href="'. get_permalink() . '" class="readmore">Lire la suite &rarr;</a>';
 }
 add_filter('excerpt_more', 'new_excerpt_more');
 
-add_theme_support( 'automatic-feed-links' );
+add_theme_support('automatic-feed-links');
